e422eb061d904f1929acbfd3075fc659b0a5a529
Débora Gama <debgama.developer@gmail.com>
2024-10-21 22:51:58 -0300

    feat: Creates validation flow for the contracts_tape table with handler and configuration yaml file.

---
+++ tables/contracts_tape/handler.py
@@ -0,0 +1,25 @@
+import os
+from services.db_validation.db_validation_service import DBValidation
+
+
+def main():
+    slack_token = os.environ.get('BOT_TOKEN')
+    slack_channel = os.environ.get('CHANNEL NAME')
+
+    config_db_dict =  {
+        'host': os.environ.get('HOST'),
+        'database': os.environ.get('DB_NAME'),
+        'user': os.environ.get('DB_USER'),
+        'password': os.environ.get('DB_PASSWORD'),
+        'port': os.environ.get('DB_PORT')
+    }
+    validator = DBValidation('contracts_tape_config.yaml',db_to_query= config_db_dict, db_to_save_config= config_db_dict, bot_token= slack_token, report_channel = slack_channel)
+    validator.run_validations_from_yaml()
+
+
+if __name__ == '__main__':
+    main()
+
+    
+
+    
